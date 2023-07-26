43fe36d5a90e38a0ce13f1fa12a6f8c63a9bbb35
RobertMargarian <margarian.robert@yahoo.com>
2023-07-25 22:57:01 -0700

    Updates on frontend

--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Company, User, Order, Client, User_Role, Plan
 
 
 def orders(request):
-    return render()
+    orders = Order.objects.all()
+    context = {
+        "orders": orders
+    }
+    return render(request, "orders.html", context)
 
