d384a2fa2bdde60c20618a0d355f68847910d6bd
RobertMargarian <margarian.robert@yahoo.com>
2023-09-13 15:43:03 -0700

    Update
    New app 'myprofile'

---
+++ myprofile/urls.py
@@ -0,0 +1,10 @@
+# from django.urls import path
+# from .views import (
+    
+# )
+
+# app_name = "myprofile"
+
+# urlpatterns = [
+#     path('myprofile/', MyProfileView.as_view(), name='myprofile'),
+# ]
