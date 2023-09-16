2a9375ada44d239901b9a68e3f231be52c3f1969
RobertMargarian <margarian.robert@yahoo.com>
2023-09-15 19:17:21 -0700

    Update
    Setting up the password reset function (not finished)

--- djticket/urls.py
+++ djticket/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path, include
 from customers.views import SignupView, LandingPageView
 
@@ -13,6 +13,10 @@ urlpatterns = [
     path('', include(('orders.urls', 'orders'), namespace="orders")),
     path('', include(('customers.urls', 'customers'), namespace="customers")),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
